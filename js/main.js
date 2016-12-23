f413bc145df7a281a236ef4f3efdd2af667e4c84
maksolimp <incognito.ukr@gmail.com>
2016-12-24 01:01:38 +0200

    firt commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+;(function($) {
+	"use strict";
+
+	$('.ba-slider').slick({
+		 infinite: true,
+  		slidesToShow: 3,
+  		slidesToScroll: 1,
+  		variableWidth: true,
+  		infinite: true
+	});
+
+})(jQuery);
+
